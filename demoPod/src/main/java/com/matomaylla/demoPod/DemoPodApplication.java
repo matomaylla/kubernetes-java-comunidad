3f1162887e43b3d8ee344cdf0cf09c30e9d0c15c
matomaylla <matomaylla@indracompany.com>
2024-07-27 23:14:05 -0500

    init demoPod

---
+++ demoPod/src/main/java/com/matomaylla/demoPod/DemoPodApplication.java
@@ -0,0 +1,13 @@
+package com.matomaylla.demoPod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPodApplication.class, args);
+	}
+
+}
---
+++ demoPod/src/main/java/com/matomaylla/demoPod/controller/PodInfoController.java
@@ -0,0 +1,20 @@
+package com.matomaylla.demoPod.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PodInfoController {
+    private final String podName;
+    private final String instanceId;
+ 
+    public PodInfoController(@Value("${POD_NAME:Unknown}") String podName) {
+        this.podName = podName;
+        this.instanceId = UUID.randomUUID().toString();
+    }
+
+    @GetMapping("/")
+    public String getPodInfo() {
+        return String.format("Pod Name: %s, Instance ID: %s", podName, instanceId);
+    }
+}
