06ac6bad15f29b6fd59aedc58e9a5ab977614435
michael tomaylla <pulser2310@gmail.com>
2024-08-08 22:17:45 -0500

    fix autowired

--- demoPod/src/main/java/com/matomaylla/demoPod/service/UserService.java
+++ demoPod/src/main/java/com/matomaylla/demoPod/service/UserService.java
@@ -2,11 +2,13 @@ package com.matomaylla.demoPod.service;
 
 import com.matomaylla.demoPod.model.User;
 import com.matomaylla.demoPod.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
     public String obtenerNombre(){
